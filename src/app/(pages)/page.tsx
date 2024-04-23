5d96dc4fba22fd341bf0f09923ef79604fc6b0ad
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-04-23 15:57:57 -0500

    Api added

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,15 +1,17 @@
-import MediaGallery from '@/components/MediaGallery';
+import MediaGallery from "@/components/MediaGallery";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default async function Home() {
+  const { resources } = await cloudinary.api.resources();
   return (
     <div className="h-full mt-6">
-      <MediaGallery
-        resources={[
-          {
-            id: 'my-image'
-          }
-        ]}
-      />
+      <MediaGallery resources={resources} />
     </div>
-  )
+  );
 }
