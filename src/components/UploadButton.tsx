5aea949e0b5b370ba834a832ec5cb25c4189a92c
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-04-24 23:31:53 -0500

    Create custom Hook

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,6 +1,18 @@
 "use client";
 
+import { Upload } from "lucide-react";
+
 import { CldUploadButton } from "next-cloudinary";
 export const UploadButton = () => {
-  return <CldUploadButton />;
+  return (
+    <CldUploadButton
+      signatureEndpoint="/api/sign-cloudinary-params"
+      options={{ autoMinimize: true }}
+    >
+      <span className="flex items-center gap-2">
+        <Upload className="w-5 h-5" />
+        Upload
+      </span>
+    </CldUploadButton>
+  );
 };
